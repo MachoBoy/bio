db9ee93b1b5092587aa7d1238cf8ed0272753b0d
Machoboy <jiiwon0@gmail.com>
2022-07-11 15:50:10 -0700

    move data into separate file

---
+++ components/skill-card/skill-card-item.ts
@@ -0,0 +1,60 @@
+const skillCardItem = [
+  {
+    icon: '/html.png',
+    name: 'HTML 5',
+  },
+  {
+    icon: '/css.png',
+    name: 'CSS',
+  },
+  {
+    icon: '/sass.png',
+    name: 'Sass',
+  },
+  {
+    icon: '/styled-components.png',
+    name: 'Styled-components',
+  },
+  {
+    icon: '/tailwindcss.png',
+    name: 'Tailwindcss',
+  },
+  {
+    icon: '/js.png',
+    name: 'JavaScript',
+  },
+  {
+    icon: '/typescript.png',
+    name: 'TypeScript',
+  },
+  {
+    icon: '/react.png',
+    name: 'React JS',
+  },
+  {
+    icon: '/vue.png',
+    name: 'Vue JS',
+  },
+  {
+    icon: '/next.png',
+    name: 'Next JS',
+  },
+  {
+    icon: '/node.png',
+    name: 'Node JS',
+  },
+  {
+    icon: '/expressjs-icon.png',
+    name: 'Express',
+  },
+  {
+    icon: '/graph.png',
+    name: 'GraphQL',
+  },
+  {
+    icon: '/firebase.png',
+    name: 'Firebase',
+  },
+];
+
+export default skillCardItem;
