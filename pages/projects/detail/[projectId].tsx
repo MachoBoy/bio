476a398638ae4d23b416f7614bfca87a2f4cc9df
Machoboy <jiiwon0@gmail.com>
2022-08-22 13:16:33 -0700

    comment out

--- pages/projects/detail/[projectId].tsx
+++ pages/projects/detail/[projectId].tsx
@@ -1,41 +1,34 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import { motion, AnimatePresence } from 'framer-motion';
-import Modal from 'react-modal';
-import { ProjectCardItem } from '../../../components/project-list/projects-card-item';
+export {};
+// import { useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import { motion, AnimatePresence } from 'framer-motion';
+// import Modal from 'react-modal';
+// import { ProjectCardItem } from '../../../components/project-list/projects-card-item';
 
-Modal.setAppElement('#__next');
+// Modal.setAppElement('#__next');
 
-const DetailPage = () => {
-  const router = useRouter();
+// const DetailPage = () => {
+//   const router = useRouter();
 
-  useEffect(() => {
-    router.prefetch('/');
-  });
+//   useEffect(() => {
+//     router.prefetch('/');
+//   });
 
-  return (
-    <>
-      <AnimatePresence>
-        <motion.div>
-          <Modal isOpen={true}>dddd</Modal>
-        </motion.div>
-      </AnimatePresence>
-    </>
-  );
-};
+//   return <></>;
+// };
 
-export default DetailPage;
+// export default DetailPage;
 
-export function getStaticProps({ params: { projectId } }: any) {
-  console.log(projectId);
-  return { props: { projectId } };
-}
+// export function getStaticProps({ params: { projectId } }: any) {
+//   // console.log(projectId);
+//   return { props: { projectId } };
+// }
 
-export function getStaticPaths() {
-  return {
-    paths: ProjectCardItem.map((project) => ({
-      params: { projectId: project.id },
-    })),
-    fallback: false,
-  };
-}
+// export function getStaticPaths() {
+//   return {
+//     paths: ProjectCardItem.map((project) => ({
+//       params: { projectId: project.id },
+//     })),
+//     fallback: false,
+//   };
+// }
